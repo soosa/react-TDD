3eeef6885be97a71de75c3e078a65f43f274cc36
Anett_Soos <anett.soos@city.ac.uk>
2017-10-30 14:09:24 +0000

    App-test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 const App = () => (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">We
+            lcome to React</h1>
         </header>
         <p className="App-intro">
           Intro
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,17 @@
 /* global test, it, expect */
-import sum from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+configure({ adapter: new Adapter() });
+
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(App(1, 2)).toBe(2);
+// });
